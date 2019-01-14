26be412b99564d6245185e50baf61061c26501f3
John Wayne VanSickle <jwvansickle@icloud.com>
2019-01-14 15:30:27 -0500

    Initial Commit

---
+++ CapsLockController/CapsLockController.h
@@ -0,0 +1,13 @@
+//
+//  CapsLockController.h
+//  CapsLockController
+//
+//  Created by John VanSickle on 1/14/19.
+//  Copyright © 2019 John VanSickle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CapsLockController : NSObject
+
+@end
---
+++ CapsLockController/CapsLockController.m
@@ -0,0 +1,13 @@
+//
+//  CapsLockController.m
+//  CapsLockController
+//
+//  Created by John VanSickle on 1/14/19.
+//  Copyright © 2019 John VanSickle. All rights reserved.
+//
+
+#import "CapsLockController.h"
+
+@implementation CapsLockController
+
+@end
